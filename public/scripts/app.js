23dac6d382f8b1ac23245d1015d868932696af33
Yourthy <christopher.t.yourth@gmail.com>
2021-01-11 22:23:34 +0000

    started initial process of working on routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,15 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+    url: "/home/1"
+  }).done((user) => {
+    console.log(user);
+    $('#user').html(`Name: ${JSON.stringify(user)}`);
+
+  });
 });
+
+
+
+
+
