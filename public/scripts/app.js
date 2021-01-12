a6274a8361c06a93953a006b12f10baf8bdcdfcf
Yourthy <christopher.t.yourth@gmail.com>
2021-01-12 04:40:39 +0000

    added jquery/ajax to pull todo from front end. Created post request to add text to todo database.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,29 @@
+/*jquery 4 step process
+1. target:
+2. addEventListener
+3(optional) Retarget:
+4. Effect
+
+
+*/
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/home/1"
-  }).done((user) => {
-    console.log(user);
-    $('#user').html(`Name: ${JSON.stringify(user)}`);
-
-  });
+  //target
+  $('#newTask').submit(function(event) {
+    event.preventDefault();
+    console.log($('#addtask').val());
+    const addTitle = $('#addtask').val();
+    $.ajax({
+      type: "POST",
+      url: "/home",
+      data: {todo_title: addTitle},
+      success: function(res) {
+        console.log(res);
+        console.log("Sucess!");
+    }
+    }).done((user) => {
+      console.log(user);
+    });
+  })
 });
-
-
-
 
 
