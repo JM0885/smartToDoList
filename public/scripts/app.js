233953b8a946f7f9107cc4e7aaeef61e3fd2ff42
JM0885 <jared.mosher@gmail.com>
2021-01-12 04:09:14 +0000

    Add further functionality and styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,22 @@ $(() => {
   });
 });
 
+//append new task input to task list
 
+$(document).ready(function () {
+$("#task_input").on('input', function(){
+  if(keyCode == 13 && $("#task_input").val() != "")
+  {
+    let task = $("<div class='task_input'></div>").text($("#addtask").val());
+    $(".tasks").append(task);
+    } 
+  });
+});
 
-
-
+$(".btn-task").on('input', function(){
+  if(keyCode == 13 && $(".btn-task").val() != "")
+  {
+    let task = $("<div class=''></div>").text($(".btn-task").val());
+    $(".tasks").append(task);
+    }
+  });
