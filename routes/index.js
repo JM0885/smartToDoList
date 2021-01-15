583509bdf20609d9362d479adcca04d3bc8eab03
Cameron <camglowicki@hotmail.com>
2021-01-15 04:54:00 +0000

    updated post route to include new values from todos table

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ const router = express.Router();
 const taskInfo = require('../services/taskInfo.js')
 const dbParams = require('../lib/db.js');
 const renderTaskElm = require('../server.js');
-const { fetchTaskInfo } = require('../services/taskInfo.js');
 
 module.exports = (db) => {
 
@@ -19,11 +18,15 @@ module.exports = (db) => {
     let category = req.body.category;
     const start_date = String(req.body.start_date);
     taskInfo.fetchTaskInfo(addTask, category, (info) => {
-    db.query(`
-    INSERT INTO todos(user_id, title, category_id, start_date, img_url)
-    VALUES(1, $1, $2, $3, $4)
+      const a = info ? info.img_url : info;
+      const b = info ? info.info_url : info;
+      const c = info ? info.author : info;
+      const d = info ? info.resto_title : info;
+      db.query(`
+    INSERT INTO todos(user_id, title, category_id, start_date, img_url, info_url, author, resto_title)
+    VALUES(1, $1, $2, $3, $4, $5, $6, $7)
     RETURNING *;
-    `,[addTask, category, start_date, info.img_url]);
+    `,[addTask, category, start_date, a, b, c, d]);
     res.redirect('/');
   })
 })
